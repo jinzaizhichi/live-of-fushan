0dd27d9842db0af84758f49ff57afc1343f40cd3
jinzaizhichi <jinzaizhichi9888@gmail.com>
2020-08-17 15:26:40 +0800

    "hebing"

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,15 @@
+//Page Object
+Page({
+  data: {
+          msg:"hello,world",
+          Num:10000,
+          isgirl:true,
+          person:{
+            age    : 74,
+            height : 145,
+            weight : 180,
+            name   :"xiaozhang"
+          }
+  },
+});
+  
