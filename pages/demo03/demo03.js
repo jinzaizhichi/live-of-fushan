cc6d8ac20d36ec7f81ec87aa012cfc4c3dd498ea
jinzaizhichi <jinzaizhichi9888@gmail.com>
2020-09-03 11:06:19 +0800

    demo

--- pages/demo03/demo03.js
+++ pages/demo03/demo03.js
@@ -1,31 +1,29 @@
 //Page Object
 Page({
-  data: {
-          msg:"hello,world",
-          Num:10000,
-          isgirl:true,
-          person:{
-            age    : 74,
-            height : 145,
-            weight : 180,
-            name   :"xiaozhang",
-      
-          },
-          list:[
-            {
-              id:0,
-              name:"八戒"
-         
-            } ,
-            {
-              id:1,
-              name:"天棚元帅"
+    data: {
+        msg: "hello,world",
+        Num: 10000,
+        isgirl: true,
+        person: {
+            age: 74,
+            height: 145,
+            weight: 180,
+            name: "xiaozhang",
+
+        },
+        list: [{
+                id: 0,
+                name: "八戒"
+
             },
             {
-              id:2,
-              name:"wuneng"
+                id: 1,
+                name: "天棚元帅"
+            },
+            {
+                id: 2,
+                name: "wuneng"
             }
-           ]       
-  },
+        ]
+    },
 });
-  
--- pages/demo04/demo04.js
+++ pages/demo04/demo04.js
@@ -1,13 +1,13 @@
 // pages/demo04/demo04.js
 Page({
 
-  data: {
-        num:0
-  },
-  handleInput(e) {
-    
-    this.setData({
-      num: e.detail.value
-    })
-  }
+    data: {
+        num: 0
+    },
+    handleInput(e) {
+
+        this.setData({
+            num: e.detail.value
+        })
+    }
 })
